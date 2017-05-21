19f76716edfc4fc57b2bbc7c16a033524ffab79c
Christoph Kurrat <christoph.kurrat@andrena.de>
2017-05-21 15:15:10 +0200

    use traverson

--- src/main/java/de/andrena/springworkshop/RestTemplateConfig.java
+++ src/main/java/de/andrena/springworkshop/RestTemplateConfig.java
@@ -2,7 +2,12 @@ package de.andrena.springworkshop;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.client.Traverson;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @Configuration
 public class RestTemplateConfig {
@@ -10,6 +15,15 @@ public class RestTemplateConfig {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	public Traverson traverson() {
+		UriComponentsBuilder apiUrlBuilder = UriComponentsBuilder.newInstance();
+		URI uri = apiUrlBuilder.scheme("http").host("localhost").port(8090).build().toUri();
+		Traverson traverson = new Traverson(uri, MediaTypes.HAL_JSON);
+		traverson.setRestOperations(restTemplate());
+		return traverson;
 	}
 
 }
--- src/main/java/de/andrena/springworkshop/dao/EventDaoImpl.java
+++ src/main/java/de/andrena/springworkshop/dao/EventDaoImpl.java
@@ -6,38 +6,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
+import org.springframework.hateoas.client.Traverson;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class EventDaoImpl implements EventDao {
 
-	private final RestTemplate restTemplate;
-
-	private String scheme = "http";
-	private String host = "localhost:8090";
+	private final Traverson traverson;
+	private RestTemplate restTemplate;
 
 	@Autowired
-	public EventDaoImpl(RestTemplate restTemplate) {
+	public EventDaoImpl(Traverson traverson, RestTemplate restTemplate) {
+		this.traverson = traverson;
 		this.restTemplate = restTemplate;
 	}
 
 
 	@Override
 	public List<EventDTO> getAllEvents() {
-		final String path = "/events";
-		final UriComponentsBuilder apiUrlBuilder = UriComponentsBuilder.newInstance();
-		final String url = apiUrlBuilder.scheme(scheme).host(host).path(path).build().toString();
-
-		Resources<Resource<EventDTO>> eventResponse = sendRequest(url, new ParameterizedTypeReference<Resources<Resource<EventDTO>>>() {
+		Resources<Resource<EventDTO>> eventResponse = traverson.follow("events").toObject(new ParameterizedTypeReference<Resources<Resource<EventDTO>>>() {
 		});
-
 		injectSpeakers(eventResponse);
 		return mapToDTOList(eventResponse);
 	}
@@ -51,14 +46,31 @@ public class EventDaoImpl implements EventDao {
 			ParameterizedTypeReference<Resources<Resource<SpeakerDTO>>> speakerType = new ParameterizedTypeReference<Resources<Resource<SpeakerDTO>>>() {
 			};
 			for (Resource<EventDTO> event : eventResponse) {
-				String speakersUrl = event.getLink("speakers").getHref();
-				Resources<Resource<SpeakerDTO>> speakerResources = sendRequest(speakersUrl, speakerType);
-				if (speakerResources != null) {
-					List<SpeakerDTO> speakerDTOList = speakerResources.getContent().stream().map(Resource::getContent).collect(Collectors.toList());
-					event.getContent().setSpeakers(speakerDTOList);
+				if (event.hasLink("speakers")) {
+					Resources<Resource<SpeakerDTO>> speakerResources = sendRequest(event.getLink("speakers").getHref(), speakerType);
+					if (speakerResources != null) {
+						List<SpeakerDTO> speakerDTOList = speakerResources.getContent().stream().map(Resource::getContent).collect(Collectors.toList());
+						event.getContent().setSpeakers(speakerDTOList);
+					}
 				}
 			}
 		}
+	}
+
+	@Override
+	public List<EventDTO> getEventsWithTitleContaining(String title) {
+		Resources<Resource<EventDTO>> eventResponse = traverson.follow("events", "search", "findByTitleContaining").withTemplateParameters(Collections.singletonMap("title", title)).toObject(new ParameterizedTypeReference<Resources<Resource<EventDTO>>>() {
+		});
+		injectSpeakers(eventResponse);
+		return mapToDTOList(eventResponse);
+	}
+
+	@Override
+	public List<EventDTO> getEventsWithDescriptionContaining(String description) {
+		Resources<Resource<EventDTO>> eventResponse = traverson.follow("events", "search", "findByDescriptionContaining").withTemplateParameters(Collections.singletonMap("description", description)).toObject(new ParameterizedTypeReference<Resources<Resource<EventDTO>>>() {
+		});
+		injectSpeakers(eventResponse);
+		return mapToDTOList(eventResponse);
 	}
 
 	private <T> T sendRequest(String url, ParameterizedTypeReference<T> type) {
@@ -67,30 +79,6 @@ public class EventDaoImpl implements EventDao {
 			return response.getBody();
 		}
 		return null;
-	}
-
-	@Override
-	public List<EventDTO> getEventsWithTitleContaining(String title) {
-		final String path = "/events/search/findByTitleContaining";
-		final UriComponentsBuilder apiUrlBuilder = UriComponentsBuilder.newInstance();
-		final String url = apiUrlBuilder.scheme(scheme).host(host).path(path).queryParam("title", title).build().toString();
-
-		Resources<Resource<EventDTO>> eventResponse = sendRequest(url, new ParameterizedTypeReference<Resources<Resource<EventDTO>>>() {
-		});
-		injectSpeakers(eventResponse);
-		return mapToDTOList(eventResponse);
-	}
-
-	@Override
-	public List<EventDTO> getEventsWithDescriptionContaining(String description) {
-		final String path = "/events/search/findByDescriptionContaining";
-		final UriComponentsBuilder apiUrlBuilder = UriComponentsBuilder.newInstance();
-		final String url = apiUrlBuilder.scheme(scheme).host(host).path(path).queryParam("description", description).build().toString();
-
-		Resources<Resource<EventDTO>> eventResponse = sendRequest(url, new ParameterizedTypeReference<Resources<Resource<EventDTO>>>() {
-		});
-		injectSpeakers(eventResponse);
-		return mapToDTOList(eventResponse);
 	}
 
 }
