4c0e521652b5e75c1cb1675faaedcb28656f043b
Bonnie Chow <bonnie.chow@andrena.de>
2017-05-21 11:35:06 +0200

    init spring data spring boot

--- src/main/java/de/andrena/springworkshop/SpringworkshopApplication.java
+++ src/main/java/de/andrena/springworkshop/SpringworkshopApplication.java
@@ -2,8 +2,11 @@ package de.andrena.springworkshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+//for jsr310 java 8 java.time.*
+@EntityScan(basePackageClasses = {SpringworkshopApplication.class})
 public class SpringworkshopApplication {
 
 	public static void main(String[] args) {
